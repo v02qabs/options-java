6bb0cee2dafbbbbb3d1eb9c507d5f60eccdc95c4
v02qabs <takesue090@gmail.com>
2024-10-19 20:47:20 +0900

    2024.10.19-20:47

--- src/com/hiro/liboptions/sample.java
+++ src/com/hiro/liboptions/sample.java
@@ -4,21 +4,25 @@ public class sample{
 	public String arg;
 	public int i;
 	public static void main(String[] args){
-		new sample().init_opt(args.length ,args);
+		new sample().init_opt(args.length ,"--push", args);
 		System.out.println("引数の個数:" + args.length);
 	}
 	public int where_options_position(int i){
 		System.out.println("Where is 'add' option is " + i);
 		return this.i = i;
 	}
-	public void init_opt(int length, String...  args){
+	public String rargs;
+	public String init_opt(int length, String options, String...  args){
 		for(int i=0;i<args.length; i++){
 			System.out.println("データの個数：" + args[i]);
-			if(args[i].equals("--add")){
+			if(args[i].equals(options)){
 				System.out.println("hit");
-				System.out.println("--add のオプション：" + args[i+1]);
+				System.out.println("[" + options + "] + のオプション：" + args[i+1]);
+				rargs = args[i+1];
+				return rargs;
 			}
 		}
+			return rargs;
 	}
 
 }
